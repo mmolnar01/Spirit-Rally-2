710d0c3df266f3ecf25fae6f84ee77ca55ef932b
Molnár Márton <127486642+mmolnar01@users.noreply.github.com>
2025-04-25 12:13:13 +0200

    Create RacepointCard.kt

---
+++ app/src/main/java/hu/klm60o/android/spiritrally2/presentation/racepoints/components/RacepointCard.kt
@@ -0,0 +1,116 @@
+package hu.klm60o.android.spiritrally2.presentation.racepoints.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
+import hu.klm60o.android.spiritrally2.SpiritRally2
+import hu.klm60o.android.spiritrally2.domain.model.Racepoint
+import hu.klm60o.android.spiritrally2.ui.theme.SpiritRally2Theme
+import java.text.SimpleDateFormat
+import java.util.Date
+
+@Composable
+fun RacepointCard(
+    racepoint: Racepoint,
+    size: Int
+) {
+    val formatter = SimpleDateFormat("hh:mm")
+    var timeText = "-"
+    if (racepoint.timestamp != null) {
+        val date = racepoint.timestamp?.toDate()
+        timeText = formatter.format(date)
+    }
+    Row(
+        modifier = Modifier
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (racepoint.id == 1) {
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "Start point icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text("Rajt")
+            }
+        } else if (racepoint.id == size) {
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "End point icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text("Cél")
+            }
+        } else {
+            val racepointNumber = racepoint.id?.minus(1)
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Place, contentDescription = "Intermediate point icon"
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text("$racepointNumber. ellenörző pont")
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(5.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            OutlinedTextField(
+                value = timeText,
+                onValueChange = {},
+                readOnly = true,
+                modifier = Modifier
+                    .width(100.dp),
+                label = {
+                    Text("Időpont")
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RacepointCardPreview() {
+    val racePoint1 = Racepoint(1, GeoPoint(1.0, 1.0), Timestamp(100000, 50000))
+    SpiritRally2Theme {
+        RacepointCard(racePoint1, 1)
+    }
+}
