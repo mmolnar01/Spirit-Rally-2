181d74c402722ed9013946ab03ad08edba30bc14
Molnár Márton <127486642+mmolnar01@users.noreply.github.com>
2025-05-18 14:40:10 +0200

    SetUserLocation added

---
+++ app/src/main/java/hu/klm60o/android/spiritrally2/presentation/userdata/components/SetUserLocation.kt
@@ -0,0 +1,10 @@
+package hu.klm60o.android.spiritrally2.presentation.userdata.components
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import hu.klm60o.android.spiritrally2.presentation.userdata.UserDataViewModel
+
+@Composable
+fun SetUserLocation(viewModel: UserDataViewModel = hiltViewModel()) {
+
+}
