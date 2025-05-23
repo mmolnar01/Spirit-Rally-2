26de58d921765f96050cae3b98cafce2e38d9907
Molnár Márton <127486642+mmolnar01@users.noreply.github.com>
2025-05-23 20:59:07 +0200

    Removed unused imports

--- app/src/main/java/hu/klm60o/android/spiritrally2/SpiritRallyMessagingService.kt
+++ app/src/main/java/hu/klm60o/android/spiritrally2/SpiritRallyMessagingService.kt
@@ -7,7 +7,6 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
